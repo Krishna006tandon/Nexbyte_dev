8789596c84e1b69832c007facb11c8269f402eb8
Krishna006tandon <krishnatandon006@gmail.com>
2025-12-27 00:25:12 +0530

    jhj

--- nexbyte-portfolio/src/pages/InternPanel.js
+++ nexbyte-portfolio/src/pages/InternPanel.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './InternPanel.css';
-import InternSidebar from '../components/InternSidebar';
 import { useAuth } from '../context/AuthContext';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
